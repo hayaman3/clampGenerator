3fd0730cbcb961d2ace57905955e68b731bf5ac4
Kevin <60098514+hayaman3@users.noreply.github.com>
2022-07-23 23:47:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let maxWidth = 52.5;
 let minFontSize = 1;
 let maxFontSize = 3.5;
 
+
 function getPrefferedValue(){
     let slope = getSlope(minWidth,maxWidth,minFontSize,maxFontSize)
     console.log("slope: "+slope);
@@ -14,3 +15,20 @@ function getPrefferedValue(){
 function getSlope(minWidth,maxWidth,minFontSize,maxFontSize){
     return (maxFontSize-minFontSize)/(maxWidth-minWidth)
 }
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // prevent page refresh
+
+    const formData = new FormData(form);
+    // const values = [...formData.entries()];
+    // console.log(values);
+
+    minWidth = formData.get("max-parent");
+    maxWidth = formData.get("min-parent");
+    minFontSize = formData.get("max-child");
+    maxFontSize = formData.get("min-child2");
+
+    getPrefferedValue()
+})
